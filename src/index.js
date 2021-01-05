028a0d19d34cb2a06ab7d930d761fbc1e1769fc7
Christian <gjerdingc@gmail.com>
2021-01-05 15:33:41 +0100

    First

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import cors from 'cors'
+import express from 'express';
+
+const app = express();
+
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
