92d7e4961b89bd9b8005ee098e7b3e0fb210d39d
Christian <gjerdingc@gmail.com>
2021-01-06 12:25:53 +0100

    Add userId parameter to URI

--- src/index.js
+++ src/index.js
@@ -7,20 +7,20 @@ const app = express();
 app.use(cors());
 
 
-app.get('/', (req, res) => {
-    res.send('Received a GET HTTP method');
+app.get('/users', (req, res) => {
+    res.send('GET HTTP method on user resource');
 });
 
-app.post('/', (req, res) => {
-  res.send('Received a POST HTTP method');
+app.post('/users', (req, res) => {
+  res.send('POST HTTP method on user resource');
 });
 
-app.put('/', (req, res) => {
-  res.send('Received a PUT HTTP method');
+app.put('/users/:userId', (req, res) => {
+  res.send(`PUT HTTP method on user/${req.params.userId} resource`);
 });
 
-app.delete('/', (req, res) => {
-  res.send('Received a DELETE HTTP method');
+app.delete('/users/:userId', (req, res) => {
+  res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
 });
 
 
