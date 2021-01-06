4946be17ece6bc70ff8a7296b04a5cf0dc493076
Christian <gjerdingc@gmail.com>
2021-01-06 12:20:10 +0100

    Add post, put and delete methods

--- src/index.js
+++ src/index.js
@@ -8,10 +8,24 @@ app.use(cors());
 
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.send('Received a GET HTTP method');
 });
+
+app.post('/', (req, res) => {
+  res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+  res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+  res.send('Received a DELETE HTTP method');
+});
+
 
 
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
 );
+
